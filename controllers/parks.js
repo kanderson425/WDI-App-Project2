f74b286181b74e6c8b8e4f3b6cc84bdd015d1cf1
Kyle Anderson <kyleanderson@Kyles-MacBook-Pro.local>
2019-02-26 11:32:36 -0600

    4) Adds looped names of all national parks : Will add href links and other park information

--- controllers/parks.js
+++ controllers/parks.js
@@ -17,14 +17,10 @@ function createPark(req, res) {
 
 function getAllParks(req, res, next) {
   // console.log('HELLO')
-    // Park.find({}).then(function(parks) {
-    //     res.status(200).json(parks);
-    // });
     var options = { method: 'GET',
     url: 'https://developer.nps.gov/api/v1/parks',
     qs: 
-     { parkCode: 'zion',
-       api_key: 'TnCegm5LiZi6JnDHecKG7ZoMwrIKhMpKzfoeekPZ',
+     { api_key: 'TnCegm5LiZi6JnDHecKG7ZoMwrIKhMpKzfoeekPZ',
        fields: 'fullName,images,states,description,url' },
     headers: 
      { 'Postman-Token': 'd0adc330-b040-4d99-bc79-fec9fdd73c97',
@@ -33,9 +29,15 @@ function getAllParks(req, res, next) {
   
   request(options, function(err, response, body) {
     var parkData = JSON.parse(body);
-    console.log(parkData.data[0].name);
-    res.render('parks/allParks', {parkData: parkData});
-    // res.send('THIS IS A TEST');
+    // console.log(parkData.data[0].name);
+    res.render('parks/allParks', {
+        //users,
+        user: req.user,
+        name: req.query.name,
+        //sortKey
+        parkData: parkData
+      });
+      console.log(parkData.data[0].name);
   });
 }
 
--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+var request = require('request');
 const parksCtrl = require('../controllers/parks');
 
 router.get('/allParks', parksCtrl.getAllParks);
