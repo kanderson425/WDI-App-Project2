1674f0777346a3aaacb3d999005d8484c0f23291
Kyle Anderson <kyleanderson@Kyles-MacBook-Pro-2.local>
2019-04-02 12:23:29 -0500

    Disabling Google Oauth for deployment to Heroku

--- config/passport.js
+++ config/passport.js
@@ -7,6 +7,8 @@ var GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_SECRET,
+    // clientID: process.env['GOOGLE_CLIENT_ID'],
+    // clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
     callbackURL: process.env.GOOGLE_CALLBACK
   },
   function(accessToken, refreshToken, profile, cb) {
